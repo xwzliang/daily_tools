b48e67ce288387598344001219414eacd8d5de2a
broliang <18706703437@163.com>
2019-03-23 23:44:43 +0800

    Add script to add cover image to a book pdf

---
+++ add_cover_image_to_pdf.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+cover_image = sys.argv[1]
+pdf = sys.argv[2]
+
+cover_pdf = cover_image.split('.')[0] + '.pdf'
+out_pdf = pdf.replace('.pdf', '_final.pdf')
+
+# Use convert to convert image to pdf
+os.system('convert {} {}'.format(cover_image, cover_pdf))
+
+# Use qpdf to concatnate cover_pdf and pdf
+os.system('qpdf --empty --pages {} {} -- {}'.format(cover_pdf, pdf, out_pdf))
+
+
