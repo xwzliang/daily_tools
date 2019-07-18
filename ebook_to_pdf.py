5b8bf8b618e73928a015dbaafe64e34cba9204f1
broliang <18706703437@163.com>
2019-07-18 16:05:06 +0800

    Modify ebook_to_pdf.py: Monitor the cwd, if new ebook added, convert automatically

--- ebook_to_pdf.py
+++ ebook_to_pdf.py
@@ -4,20 +4,33 @@ import sys
 import os
 import re
 
+def convert_book(ebook):
+    # Remove invalid chars
+    ebook_replace_invalid_chars = re.sub('[^A-Za-z0-9._-]', '_', ebook)
+    pdf_name = ebook_replace_invalid_chars.split('.')[0] + '.pdf'
+    os.rename(ebook, ebook_replace_invalid_chars)
+    # use Calibre ebook converter to convert to pdf
+    os.system("ebook-convert {} {} --pdf-page-numbers --pretty-print --minimum-line-height=1.5 --change-justification='justify'".format(ebook_replace_invalid_chars, pdf_name))
+
+
 if len(sys.argv) > 1:
     ebook = sys.argv[1]
+    convert_book(ebook)
 else:
-    # get the latest file as ebook
     import glob
+    import time
     cwd = os.getcwd()
-    list_of_files = glob.glob(os.path.join(cwd, '*'))
-    latest_file = max(list_of_files, key=os.path.getctime)
-    ebook = os.path.basename(latest_file)
+    old_list_of_files = os.listdir(cwd)
 
-# Remove invalid chars
-ebook_replace_invalid_chars = re.sub('[^A-Za-z0-9._-]', '_', ebook)
-pdf_name = ebook_replace_invalid_chars.split('.')[0] + '.pdf'
-os.rename(ebook, ebook_replace_invalid_chars)
+    while True:		# Monitor the cwd
+        time.sleep(2)
+        new_list_of_files = os.listdir(cwd)
+        if len(new_list_of_files) > len(old_list_of_files):	# New file added
+            # get the latest file as ebook
+            ebook = max(new_list_of_files, key=os.path.getctime)
+            if '.pdf' not in ebook:	# Don't convert pdf file
+                convert_book(ebook)
+                time.sleep(2)
+        # Update the list of files
+        old_list_of_files = os.listdir(cwd)
 
-# use Calibre ebook converter to convert to pdf
-os.system("ebook-convert {} {} --pdf-page-numbers --pretty-print --minimum-line-height=1.5 --change-justification='justify'".format(ebook_replace_invalid_chars, pdf_name))
