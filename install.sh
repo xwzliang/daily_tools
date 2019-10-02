397161e70f3fbe0da6c3f91d5e1ce41a73351870
broliang <18706703437@163.com>
2019-10-02 17:12:09 +0800

    Add install.sh: Delete broken links from my_bin_dir

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@
 dir="$(dirname $(realpath "$0"))"
 my_bin_dir=$HOME/bin
 mkdir -p $my_bin_dir
-echo $dir
 
 scripts=$(ls $dir | grep -v $0 | grep -v README)
 
+# Delete broken links from my_bin_dir
+find $my_bin_dir -type l | perl -nle '-e || print' | xargs rm
+
 while read script; do
-	ln -s $dir/$script $my_bin_dir/$script
+	ln -sf $dir/$script $my_bin_dir/$script
 done <<< "$scripts"
 
 echo "Install Finished."
