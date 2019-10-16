3b4b46b47d8e837af716d0776d6ad24de157e5ae
broliang <18706703437@163.com>
2019-10-16 09:20:08 +0800

    Modify install.sh: Use -f for rm to ignore error

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir -p $my_bin_dir
 scripts=$(ls $dir | grep -v $0 | grep -v README)
 
 # Delete broken links from my_bin_dir
-find $my_bin_dir -type l | perl -nle '-e || print' | xargs rm
+find $my_bin_dir -type l | perl -nle '-e || print' | xargs rm -f
 
 while read script; do
 	ln -sf $dir/$script $my_bin_dir/$script
